8c84b1b10b2b9f56636a2600b9a7f1b53bc5ef8d
Sébastien De_Marco <sdemarco@hotmail.fr>
2023-10-04 12:24:54 +0200

    Ajout de la base de données

---
+++ Fenyx-Project/Fenyx-Project.WebService/Models/ModelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Fenyx_Project.WebService.Models
+{
+    public class ModelDbContext : DbContext
+    {
+        public ModelDbContext()
+            : base("name=ModelDbContext")
+        {
+        }
+    }
+}
