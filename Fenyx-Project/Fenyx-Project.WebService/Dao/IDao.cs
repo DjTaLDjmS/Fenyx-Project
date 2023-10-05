c17495dbd98ab8b696f7b335ca356b7cb4f1a538
Sébastien De_Marco <sdemarco@hotmail.fr>
2023-10-05 11:16:04 +0200

    Ajout de la couche DAO

---
+++ Fenyx-Project/Fenyx-Project.WebService/Dao/IDao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fenyx_Project.WebService.Dao
+{
+    public interface IDao<TEntity> where TEntity : class, new()
+    {
+        List<TEntity> GetAll();
+        
+        TEntity Get(Guid id);
+
+        TEntity Create(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        void Delete(Guid id);
+    }
+}
---
+++ Fenyx-Project/Fenyx-Project.WebService/Dao/Impl/UserDaoImpl.cs
@@ -0,0 +1,52 @@
+﻿using Fenyx_Project.WebService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Fenyx_Project.WebService.Dao.Impl
+{
+    public class UserDaoImpl : IDao<User>
+    {
+        private readonly ModelDbContext _dbContext;
+
+        public UserDaoImpl()
+        {
+            _dbContext = new ModelDbContext();
+        }
+        
+        public List<User> GetAll()
+        {
+            return _dbContext.Users.ToList();
+        }
+
+        public User Get(Guid id)
+        {
+            return _dbContext.Users.Include(u => u.Address).FirstOrDefault(u => u.Id == id);
+        }
+
+        public User Create(User entity)
+        {
+            _dbContext.Users.Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public User Update(User entity)
+        {
+            _dbContext.Users.AddOrUpdate(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(Guid id)
+        {
+            User entity = Get(id);
+            _dbContext.Users.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
